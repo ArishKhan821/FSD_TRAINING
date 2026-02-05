06b03afa31d88beefe8f84599e52182da922facd
ArishKhan821 <arishboss1221@gmail.com>
2026-02-05 12:37:19 +0530

    done

---
+++ JAVA_SCRIPT/if_else.js
@@ -0,0 +1,7 @@
+let age = 18;
+if(age<=20){
+    console.log("You are not allowed to get married")
+}
+else{
+    console.log("Your age is legal to get married");
+}
---
+++ JAVA_SCRIPT/switchStatement.js
@@ -0,0 +1,33 @@
+let n = 6;
+switch(n){
+    case(1):
+    console.log("Monday");
+    break;
+
+    case(2):
+    console.log("Monday");
+    break;
+    
+     case(3):
+    console.log("Tuesday");
+    break;
+
+    case(4):
+    console.log("wednesday");
+    break;
+
+    case(5):
+    console.log("thursday");
+    break;
+
+    case(6):
+    console.log("friday");
+    break;
+
+    case(7):
+    console.log("saturday");
+    break;
+    default:
+    console.log("please enter number from 1 to 7");
+
+}
