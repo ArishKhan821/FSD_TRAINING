c12cb01e08e3e3296c616244b037485a23eef23d
ArishKhan821 <arishboss1221@gmail.com>
2026-02-09 09:40:32 +0530

    done

---
+++ JAVA_SCRIPT/function.js
@@ -0,0 +1,80 @@
+// function add(){
+//     let num1=23;
+//     num2=34;
+//     console.log(num1+num2);
+// }
+// add()
+
+// add()
+// add(4,5)
+// add(10,20)
+// function add(a,b){
+//     let num1=a;
+//     num2=b;
+//     console.log(num1+num2);
+// }
+
+
+// function add(a,b){
+//     let num1=a;
+//     num2=b;
+//     return(num1+num2);
+// }
+// let sum = add(3,4)
+// console.log(`sum of two numbers is ${add(3,4)}`);
+// console.log(add(4,5));
+// console.log(10,20);
+// console.log(add());
+// console.log(`sum of two numbers is ${sum}`);
+
+
+// const sum = function add(a,b){            // function expression
+//     let num1=a;
+//     num2=b;
+//     return(num1+num2);
+// }
+//  console.log(`sum of two numbers is ${sum}`);
+
+
+// const sum = function add(a,b){
+//     let num1=a;
+//     num2=b;
+//     return(num1+num2);
+// }
+//  console.log(`sum of two numbers is ${sum(10,20)}`);
+
+
+// console.log(`sum of two numbers is ${sum(10,20)}`);  // will give error 
+// const sum = function add(a,b){
+//     let num1=a;
+//     num2=b;
+//     return(num1+num2);
+// }
+
+
+
+
+
+// const msg=()=>{
+//     console.log("Message to be printed ");
+// } 
+// msg(); 
+
+
+
+// const msg=s=>{
+//     return(`Message to be printed ${(s)}`);
+// } 
+// console.log(msg(6));
+
+ 
+// function greet(){
+//     console.log("Hello");
+// }
+// greet();
+
+ 
+const greet =()=>{
+    console.log("Hello");
+}
+greet();
